7c4b34ccef1310a44d3542d35da7a58b7c1b955c
FV214Hardiel <darthpanther@gmail.com>
2023-03-18 16:13:08 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GutHubTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TESTING 1");
+            Console.ReadLine();
+        }
+    }
+}
